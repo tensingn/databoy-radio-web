100f21af894408065ed393bb979665a07e141774
tensingn <tensingn@oregonstate.edu>
2022-02-25 08:08:08 -0500

    working on ui for store's product list

---
+++ frontend/src/app/store/components/product-card/product-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../../interfaces/product';
+
+@Component({
+  selector: 'store-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+})
+export class ProductCardComponent implements OnInit {
+  @Input() product: Product;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ frontend/src/app/store/services/product-service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../interfaces/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private _url: string = 'url-to-products';
+  private _products: Product[] = [];
+
+  constructor() {}
+
+  getProducts(): Product[] {
+    return mockProducts;
+  }
+}
+
+let mockProducts: Product[] = [
+  {
+    id: 1,
+    name: 'Shirt',
+    type: 'Clothing',
+    price: 19.99,
+    img: 'https://cdni.llbean.net/is/image/wim/224547_1_41?hei=1092&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2',
+  },
+  {
+    id: 2,
+    name: 'Hat',
+    type: 'Acccessories',
+    price: 12.99,
+    img: 'https://media.kohlsimg.com/is/image/kohls/3527811_Black?wid=1200&hei=1200&op_sharpen=1',
+  },
+  {
+    id: 3,
+    name: 'Mixtape',
+    type: 'Music',
+    price: 9.99,
+    img: 'https://hub.yamaha.com/wp-content/uploads/2021/04/mixtape-cassette-image_1400-1024x544.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sweatpants',
+    type: 'Clothing',
+    price: 29.99,
+    img: 'https://cdni.llbean.net/is/image/wim/512293_50118_41?hei=1092&wid=950&resMode=sharp2&defaultImage=llbprod/A0211793_2',
+  },
+  {
+    id: 5,
+    name: 'Hoodie',
+    type: 'Clothing',
+    price: 39.99,
+    img: 'https://imgprd19.hobbylobby.com/3/41/22/34122aa4291387b735fc61c2e614d6a038fd0102/350Wx350H-1385194-0320.jpg',
+  },
+];
