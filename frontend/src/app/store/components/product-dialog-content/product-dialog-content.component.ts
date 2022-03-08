22884a4812c4876a2734cad9b6639ff42e5df930
tensingn <tensingn@oregonstate.edu>
2022-03-08 08:20:28 -0500

    working on StoreService

--- frontend/src/app/store/components/product-dialog-content/product-dialog-content.component.ts
+++ frontend/src/app/store/components/product-dialog-content/product-dialog-content.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CartItem } from '../../interfaces/cart-item';
 import { Product } from '../../interfaces/product';
+import { StoreService } from '../../services/store-service';
 
 export interface DialogData {
   product: Product;
@@ -15,7 +17,8 @@ export interface DialogData {
 export class ProductDialogContentComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<ProductDialogContentComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private storeService: StoreService
   ) {}
 
   ngOnInit(): void {}
@@ -26,5 +29,10 @@ export class ProductDialogContentComponent implements OnInit {
 
   onAddToCartClick(quantity: string): void {
     console.log(`add ${quantity} to cart`);
+    let itemToAdd: CartItem = {
+      quantity: +quantity,
+      product: this.data.product,
+    };
+    this.storeService.addToCart(itemToAdd);
   }
 }
