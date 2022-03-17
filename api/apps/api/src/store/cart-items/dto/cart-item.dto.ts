4ee83911060404c2c394006da3c42bc362453ecd
tensingn <tensingn@oregonstate.edu>
2022-03-17 07:23:37 -0400

    restructuring repository

--- api/apps/api/src/store/cart-items/dto/cart-item.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Size } from '../../enums/sizes.enum';
-import { ProductDto } from '../../products/dto/product.dto';
-
-export class CartItem {
-  id: number;
-  product: ProductDto;
-  quantity: number;
-  size: Size;
-}
