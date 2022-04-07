a573e23f14037ee23e3fdf8d0582d326adddd8f6
tensingn <tensingn@oregonstate.edu>
2022-04-07 06:51:48 -0400

    updating mix font size on small screen. also updating play button to be icon button rather than normal button

--- src/app/shared/components/play-button/play-button.component.ts
+++ src/app/shared/components/play-button/play-button.component.ts
@@ -1,10 +1,4 @@
-import {
-	Component,
-	Input,
-	OnChanges,
-	OnInit,
-	SimpleChanges,
-} from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Mix } from "../../../mixes/interfaces/mix";
 import { PlayerService } from "../../../template/services/player-service";
 
