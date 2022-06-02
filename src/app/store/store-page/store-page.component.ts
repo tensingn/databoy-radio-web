06202b2c0cd4e5a19a20e496cb20c3d2181c3fa9
tensingn <tensingn@oregonstate.edu>
2022-06-02 07:03:26 -0400

    setting up snipcart. can add items to cart

--- src/app/store/store-page/store-page.component.ts
+++ src/app/store/store-page/store-page.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-store-page',
-  templateUrl: './store-page.component.html',
-  styleUrls: ['./store-page.component.scss']
+	selector: "app-store-page",
+	templateUrl: "./store-page.component.html",
+	styleUrls: ["./store-page.component.scss"],
 })
 export class StorePageComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
