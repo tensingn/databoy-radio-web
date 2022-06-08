cf95125c9c4ca8800f4a388d80c49f0d25da0bcc
tensingn <tensingn@oregonstate.edu>
2022-06-08 07:27:45 -0400

    snipcart size options working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	apiBaseUrl: "https://databoy-radio-api-stage.herokuapp.com/api/",
 	productsBaseUrl:
-		"https://databoy-radio-api-stage.herokuapp.com/store/products/",
+		"https://databoy-radio-api-stage.herokuapp.com/api/products/",
 };
