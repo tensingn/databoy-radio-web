6dff5d1902b8a7169d57f241c0443235f19e2328
tensingn <tensingn@oregonstate.edu>
2022-08-02 07:42:02 -0400

    adding routing to individual event pages

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,40 +1,42 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
 
 // components
-import { EventsPageComponent } from './events-page/events-page.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { EventDialogContentComponent } from './components/event-dialog-content/event-dialog-content.component';
+import { EventsPageComponent } from "./events-page/events-page.component";
+import { CalendarComponent } from "./components/calendar/calendar.component";
+import { EventDialogContentComponent } from "./components/event-dialog-content/event-dialog-content.component";
 
 // material modules
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatCardModule } from "@angular/material/card";
 
 // other modules
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from "../shared/shared.module";
+import { EventPageComponent } from "./components/event-page/event-page.component";
 
 const routes: Routes = [
-  { path: '', component: EventsPageComponent },
-  { path: '**', redirectTo: '' },
+	{ path: ":id", component: EventPageComponent },
+	{ path: "", component: EventsPageComponent },
+	{ path: "**", redirectTo: "" },
 ];
 
 @NgModule({
-  declarations: [
-    EventsPageComponent,
-    CalendarComponent,
-    EventDialogContentComponent,
-  ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
-    MatCardModule,
-    SharedModule,
-  ],
+	declarations: [
+		EventsPageComponent,
+		CalendarComponent,
+		EventDialogContentComponent,
+	],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes),
+		MatDatepickerModule,
+		MatNativeDateModule,
+		MatDialogModule,
+		MatCardModule,
+		SharedModule,
+	],
 })
 export class EventsModule {}
--- src/app/shared/components/event-expansion-panel/event-expansion-panel.component.ts
+++ src/app/shared/components/event-expansion-panel/event-expansion-panel.component.ts
@@ -1,10 +1,4 @@
-import {
-	Component,
-	Input,
-	OnInit,
-	ViewChild,
-	ViewContainerRef,
-} from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { CalendarEvent } from "src/app/events/interfaces/calendar-event";
 
 @Component({
