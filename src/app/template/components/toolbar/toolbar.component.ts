4d0289164346ad1d612492911f82243cbc132ec3
tensingn <tensingn@oregonstate.edu>
2023-07-07 23:20:01 -0400

    testing new remote setup;

--- src/app/template/components/toolbar/toolbar.component.ts
+++ src/app/template/components/toolbar/toolbar.component.ts
@@ -1,18 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+	selector: "app-toolbar",
+	templateUrl: "./toolbar.component.html",
+	styleUrls: ["./toolbar.component.scss"],
 })
 export class ToolbarComponent implements OnInit {
-  @Input() screenIsSmall: boolean = false;
-  @Input() screenIsMediumOrLess: boolean = false;
+	@Input() screenIsSmall: boolean = false;
+	@Input() screenIsMediumOrLess: boolean = false;
 
-  @Output() toggleSidenav = new EventEmitter();
-  @Output() toggleEvents = new EventEmitter();
+	@Output() toggleSidenav = new EventEmitter();
+	@Output() toggleEvents = new EventEmitter();
 
-  constructor() {}
+	constructor() {}
 
-  ngOnInit(): void {}
+	ngOnInit(): void {}
 }
