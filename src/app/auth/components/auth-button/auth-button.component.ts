e4442ef29a027eb76fda0ddb5d4a8d66ac948320
tensingn <tensingn@oregonstate.edu>
2023-07-29 09:57:17 -0400

    switching auth button layout depending on screen size

--- src/app/auth/components/auth-button/auth-button.component.ts
+++ src/app/auth/components/auth-button/auth-button.component.ts
@@ -1,15 +1,35 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import { AuthService } from "@auth0/auth0-angular";
 import { DOCUMENT } from "@angular/common";
+import {
+	BreakpointObserver,
+	BreakpointState,
+	Breakpoints,
+} from "@angular/cdk/layout";
 
 @Component({
 	selector: "app-auth-button",
 	templateUrl: "auth-button.component.html",
 	styles: [],
 })
-export class AuthButtonComponent {
+export class AuthButtonComponent implements OnInit {
+	screenIsMediumOrLess: boolean = false;
+
 	constructor(
 		@Inject(DOCUMENT) public document: Document,
-		public auth: AuthService
+		public auth: AuthService,
+		private breakpointObserver: BreakpointObserver
 	) {}
+
+	ngOnInit() {
+		this.breakpointObserver
+			.observe([
+				Breakpoints.Medium,
+				Breakpoints.Small,
+				Breakpoints.XSmall,
+			])
+			.subscribe((state: BreakpointState) => {
+				this.screenIsMediumOrLess = state.matches;
+			});
+	}
 }
